7b4973290f8f15795333fa5ad451b4b7d5ed86fb
Timothy-Wallis <timothywallis@ucmerced.edu>
2026-05-21 22:14:09 -0700

    Fixing a fix

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,16 +1,16 @@
 import Header from "./assets/Header"
 interface SettingsProps {
-    onNavigate: (page:  'home' | 'analytics' | 'settings') => void;
-    sidebarOpen: boolean;
-    onToggleSidebar: () => void;
+  onNavigate: (page: 'home' | 'analytics' | 'settings' | 'login') => void;
+  sidebarOpen: boolean;
+  onToggleSidebar: () => void;
 }
 export default function Settings(props: SettingsProps) {
-    return (
-        <div style={{paddingLeft: '20px'}}>
-            <Header onNavigate={props.onNavigate} onToggleSidebar={props.onToggleSidebar}/>
-            <h1 style={{paddingTop: '60px'}}>Settings</h1>
-            <p>This is the settings page. Here you can manage your application settings and preferences.</p>
-            <p>More features will be added soon!</p>
-        </div>
-    )
+  return (
+    <div style={{ paddingLeft: '20px' }}>
+      <Header onNavigate={props.onNavigate} onToggleSidebar={props.onToggleSidebar} />
+      <h1 style={{ paddingTop: '60px' }}>Settings</h1>
+      <p>This is the settings page. Here you can manage your application settings and preferences.</p>
+      <p>More features will be added soon!</p>
+    </div>
+  )
 }
